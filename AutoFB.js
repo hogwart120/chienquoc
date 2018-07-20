d8fe4c120287e65f4b9b4f4093added75a66f5e3
hogwart120 <msi.official.vn@gmail.com>
2018-07-21 00:28:22 +0700

    Update AutoFB.js

--- AutoFB.js
+++ AutoFB.js
@@ -1,20 +1,17 @@
 var login = require("facebook-chat-api");
 var statusFB = {};
 login({email: "msi.official.vn@gmail.com", password: "kid01666462766"}, function callback (err, api) {
+	if(err) return console.error(err);
     api.listen(function callback(err, message) {
-        console.log(message.threadID);
-		api.setOptions({listenEvents: true});
-			switch(event.type) {
-            case "message":
-                if(event.body === '/on'){
+        console.log(message.body);
+                if(message.body === '/on'){
 					statusFB[0] = "ON";
-                }else if(event.body === '/off'){
+                }else if(message.body === '/off'){
 					statusFB[0] = "OFF";
                 }else{
 					if(statusFB[0] == "ON"){
 						api.sendMessage("Hiện tại tôi không thể trả lời tin nhắn!", message.threadID);
 					}
 				}
-			}
 		});	
 	});
